1d72d322267eb7a8d2fe1ce681677f13113e796e
Ali Hassan <hassan.jutt646464@gmail.com>
2024-10-13 10:47:03 +0500

    first commit

---
+++ lib/utils.dart
@@ -0,0 +1,34 @@
+import 'dart:io';
+
+///dartui: library provides the essential tools and APIs for
+///creating visually appealing and performant user interfaces
+import 'dart:ui';
+
+///image picker: provides a simple API for picking images from a user's device
+import 'package:image_picker/image_picker.dart';
+
+///this function takes a color and makes it "stronger" by increasing its intensity.
+///clamp method to ensure the values stay within the range.
+Color strengthenColor(Color color, double factor) {
+  int r = (color.red * factor).clamp(0, 255).toInt();
+  int g = (color.green * factor).clamp(0, 255).toInt();
+  int b = (color.blue * factor).clamp(0, 255).toInt();
+  return Color.fromARGB(color.alpha, r, g, b);
+}
+
+String rgbToHex(Color color) {
+  return '${color.red.toRadixString(16).padLeft(2, '0')}${color.green.toRadixString(16).padLeft(2, '0')}${color.blue.toRadixString(16).padLeft(2, '0')}';
+}
+
+Color hexToColor(String hex) {
+  return Color(int.parse(hex, radix: 16) + 0xFF000000);
+}
+
+Future<File?> selectImage() async {
+  final imagePicker = ImagePicker();
+  XFile? file = await imagePicker.pickImage(source: ImageSource.gallery);
+  if (file != null) {
+    return File(file.path);
+  }
+  return null;
+}
